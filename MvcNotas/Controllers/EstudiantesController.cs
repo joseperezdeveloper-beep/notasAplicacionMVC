342dc2f070b295ab9a49b300947c2a35256ddcaf
JosePerez <joseperez.developer@gmail.com>
2025-08-13 23:57:10 -0600

    Subida de archivos

---
+++ MvcNotas/Controllers/EstudiantesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcNotas.Controllers
+{
+    public class EstudiantesController: Controller
+    {
+        private readonly IConfiguration _config;
+        public EstudiantesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // Vista principal con el formulario y la tabla
+        public IActionResult Index()
+        {
+            // Pasamos la URL base de la API a la vista
+            ViewBag.ApiBase = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+    }
+}
---
+++ MvcNotas/Controllers/NotasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcNotas.Controllers
+{
+    public class NotasController:Controller
+    {
+        private readonly IConfiguration _config;
+        public NotasController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ApiBase = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+    }
+}
